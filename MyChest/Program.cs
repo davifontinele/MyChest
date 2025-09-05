47c8ee263df5108e5f64a6cf40982a3894b3a28d
Davi <davifontineledevi@gmail.com>
2025-09-04 21:28:08 -0300

    :sparkles:feat: Implementando funcionaliadade de cadastro de endereços
    Adicionando nova funcionalidade de cadastro de endereços

--- MyChest/Program.cs
+++ MyChest/Program.cs
@@ -14,7 +14,7 @@ namespace MyChest
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new AddAddressForm());
+            Application.Run(new LoginForm());
         }
     }
 }
